047a01519d7dc4a9e242d1b54d3ef3a2822f1221
Joseph Olu <josepholuolofinte@gmail.com>
2025-05-16 00:30:40 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+
+import express from 'express';
+import http from 'http';
+import compression from "compression";
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('Server is running on port 8080');
+})
