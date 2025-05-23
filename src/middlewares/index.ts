11c767c1037bb2d9114d6dc30238087e18aab59c
Joseph Olu <josepholuolofinte@gmail.com>
2025-05-23 05:54:52 +0100

    update

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,6 +2,26 @@ import express from 'express';
 import { get, merge } from 'lodash';
 import { getUserBySessionToken } from '../db/users';
 
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, "identity._id") as string;
+
+        if(!currentUserId) {
+            return res.status(403).json({ message: "Error! You are not allowed to perform this action!" });
+        }
+
+        if(currentUserId.toString() !== id) {
+            return res.status(403).json({ message: "Error! you are not properly authenticated!" });
+        }
+
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: "Account not found" });
+    }
+}
+
 export const isAuthenticated = async( req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
         const sessionToken = req.cookies["JOSEPH-AUTH"];
