b1ac60c6abc7e6a5ffec350ed9930f77ab75b031
Joseph Olu <josepholuolofinte@gmail.com>
2025-05-23 06:14:12 +0100

    update

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { deleteuserById, getUsers } from '../db/users';
+import { deleteuserById, getUserById, getUsers } from '../db/users';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
         const users = await getUsers();
 
-        return res.status(200).json(users);
+        return res.status(200).json({message: "Successfully fetched all users!", users}).end;
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Internal server error" });
@@ -18,7 +18,28 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
 
         const deletedUser = await deleteuserById(id);
 
-        return res.json(deletedUser);
+        return res.json({deletedUser, message: "Successfully deleted user!"}).end;
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: "Error! Check if user id exists." });
+    }
+}
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        const { username } = req.body;
+
+        if(!username) {
+            return res.status(400).json({ message: "Please provide a username" });
+        }   
+
+        const user = await getUserById(id);
+
+        user.username = username;
+        await user.save();
+
+        return res.status(200).json({user, message: "Successfully update username!"}).end;
     } catch (error) {
         console.log(error);
         return res.status(400).json({ message: "Error! Check if user id exists." });
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { deleteUser, getAllUsers } from '../controllers/users';
+import { deleteUser, getAllUsers, updateUser } from '../controllers/users';
 import { isAuthenticated, isOwner } from '../middlewares';
 
 export default (router: express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
     router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
+    router.patch('/users/:id', isAuthenticated, isOwner, updateUser);
 };
 
 // This file defines the routes for the users. It imports the necessary modules and functions, and then exports a function that takes a router as an argument. The function defines a GET route for '/users' that is protected by the isAuthenticated middleware and calls the getAllUsers controller function.
